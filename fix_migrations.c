25a34b94f3b5ea325b20c7d49f014d41b15e6dca
Knut Olav Bøhmer <bohmer@gmail.com>
2020-09-13 20:27:01 +0200

    initial

---
+++ fix_migrations.c
@@ -0,0 +1,194 @@
+#include <git2.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <regex.h>        
+#include <string.h>
+
+typedef struct {
+  char a;
+} status_data;
+
+
+typedef struct {
+  int major;
+  int minor;
+} version;
+
+int version_from_string(version *v, const char* s) {
+
+  int idx = 0;
+  
+  int major = atoi(s + 1);
+  while (*s++ != '_') idx++;      
+  
+  int minor = atoi(s);
+  
+  if (major > v->major) {
+    v->major = major;
+    v->minor = minor;
+    return 2;
+  } else if (v->major == major && minor > v->minor) {
+    v->minor = minor;
+    return 1;
+  }
+  
+  return 0;
+}
+
+int comp_version(version* v1, version* v2)
+{
+  
+  if(v1->major == v2->major && v1->minor > v2->minor){
+    return 1;
+  }
+
+  if(v1->major == v2->major && v1->minor < v2->minor){
+    return -1;
+  }
+
+  if(v1->major > v2->major){
+    return 1;
+  }
+
+  if(v1->major < v2->major){
+    return -1;
+  }
+
+  return 0;
+}
+
+typedef struct { 
+  char* name; 
+  struct Node* next;
+} list; 
+
+typedef struct  {
+  struct list *filelist;
+} diff_data;
+
+
+int file_cb(const git_diff_delta *delta,
+                 float progress,
+                 void *payload)
+{
+  version *v = (version*) payload;
+  version this_version = {0,0};
+  (void)progress;
+  const char* path = delta->new_file.path;
+
+  if(git_oid_iszero(&delta->new_file.id)) {
+    
+    if(path[0] == 'V'){
+      version_from_string(&this_version, path);
+      if(comp_version(v, &this_version)){
+        if(v->minor == 99) {
+          v->major++;
+          v->minor = 0;
+        } else {
+          v->minor++;        
+        }
+        printf("mv %s V%d_%02d%s\n", path, v->major, v->minor, strstr(path, "__"));
+      }
+    }
+  }
+  return 0;
+}
+
+int max_versoin_in_tree(version* version, const git_tree *tree)
+{
+  size_t i, max_i = (int)git_tree_entrycount(tree);
+  char oidstr[GIT_OID_HEXSZ + 1];
+  const git_tree_entry *te;
+
+  version->major = 0;
+  version->minor = 0;
+  
+  for (i = 0; i < max_i; ++i) {
+    te = git_tree_entry_byindex(tree, i);
+
+    git_oid_tostr(oidstr, sizeof(oidstr), git_tree_entry_id(te));
+
+    const char* filename = git_tree_entry_name(te);
+
+    if(filename[0] == 'V') {
+      version_from_string(version, filename);
+    } 
+  }
+
+  return 0;
+}
+
+
+int main(int argc, char* argv[]) {
+
+  git_repository *repo = NULL;
+  
+  git_libgit2_init();
+  char* path;
+
+  if (argc > 1) {
+    path = argv[1];
+  } else {
+    path = "/home/knobo/prog/testgit";
+  }
+           
+  int init_error = git_repository_init(&repo, path, false);
+
+  if(init_error) {
+    printf("Could not init repo");
+    exit(-1);
+  }
+  
+  git_object *this_tree = NULL;
+  int revparse_head_error = git_revparse_single(&this_tree, repo, "HEAD:resources/db/migrations");
+
+  if(revparse_head_error) {
+      const git_error *e = git_error_last();
+      printf("revparse_head_error error %d\n%s\n", revparse_head_error, e->message);
+      exit(-1);
+  }
+  
+  git_object *that_tree = NULL;
+  int revparse_master_error = git_revparse_single(&that_tree, repo, "master:resources/db/migrations");
+
+  version version = {0,0};
+  int version_error = max_versoin_in_tree(&version, (const git_tree*)that_tree);
+
+  if(version_error) {
+    printf("Could not find any versions\n");
+  }
+  
+  printf("Max version %d-%d\n", version.major, version.minor);
+  
+  if(revparse_master_error) {
+      const git_error *e = git_error_last();
+      printf("revparse_master_error error %d\n%s\n", revparse_master_error, e->message);
+      exit(-1);
+  }
+
+  
+  git_diff *diff;  
+  int diff_error = git_diff_tree_to_tree(&diff, repo, (git_tree*)this_tree, (git_tree*)that_tree, NULL /* &opts */);
+  
+  if(diff_error) {
+      const git_error *e = git_error_last();
+      printf("git_diff_tree_to_tree error %d\n%s\n", diff_error, e->message);
+      exit(-1);
+  }
+    
+  int for_each_error = git_diff_foreach(diff, file_cb, NULL, NULL, NULL, &version);
+
+  printf("For each done\n");
+  
+  if(for_each_error) {
+    const git_error *e = git_error_last();
+
+    printf("For each error %d\n%s\n", for_each_error, e->message);
+    exit(-1);
+  }
+      
+  
+  git_libgit2_shutdown();
+  
+}
